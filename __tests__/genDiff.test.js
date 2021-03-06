89d5aae792cfc394e3da30fcdabd6d69a9363850
Anton Lipilin <antonlipilinwork@gmail.com>
2021-03-06 19:14:22 +0400

    add tests for parsers.js

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,4 +1,3 @@
-import fs from 'fs';
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
 import { expect, test } from '@jest/globals';
@@ -7,8 +6,20 @@ import genDiff from '../src/genDiff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-test('difference generator', () => {
-  expect(genDiff(fs.readFileSync(`${__dirname}/../__fixtures__/file1.json`, 'utf-8'), fs.readFileSync(`${__dirname}/../__fixtures__/file2.json`, 'utf-8')))
+test('JSON simple object', () => {
+  expect(genDiff(`${__dirname}/../__fixtures__/file1.json`, `${__dirname}/../__fixtures__/file2.json`))
+    .toEqual(`{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`);
+});
+
+test('YAML simple object', () => {
+  expect(genDiff(`${__dirname}/../__fixtures__/file1.yml`, `${__dirname}/../__fixtures__/file2.yml`))
     .toEqual(`{
   - follow: false
     host: hexlet.io

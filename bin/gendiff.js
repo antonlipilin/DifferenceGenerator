97c238940977288214cdf55034ae70185d545df7
Anton Lipilin <antonlipilinwork@gmail.com>
2021-03-26 19:58:35 +0400

    add stylish as default formatter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,7 +6,7 @@ const program = new Command();
 program.version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .action((filepath1, filepath2) => {
     console.log(genDiff(filepath1, filepath2));
   });

4c40d25037b77b05f4b3e6df31b93454e6684e6b
Anton Lipilin <antonlipilinwork@gmail.com>
2021-03-26 18:13:46 +0400

    add tests & fixtures for yaml nested  objects

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -29,3 +29,97 @@ test('YAML simple object', () => {
   + verbose: true
 }`);
 });
+
+test('JSON nested object', () => {
+  expect(genDiff(`${__dirname}/../__fixtures__/nestedObject1.json`, `${__dirname}/../__fixtures__/nestedObject2.json`)).toEqual(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`);
+});
+
+test('YAML nested object', () => {
+  expect(genDiff(`${__dirname}/../__fixtures__/nestedObject1.yml`, `${__dirname}/../__fixtures__/nestedObject2.yml`)).toEqual(`{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow: 
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`);
+});

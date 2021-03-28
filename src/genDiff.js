dc190125a9e31fdba5a46121105f41bd1701cae0
Anton Lipilin <antonlipilinwork@gmail.com>
2021-03-28 17:09:03 +0400

    add formatter

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,13 +1,13 @@
 /* eslint-disable no-useless-concat */
 import parsers from './parsers.js';
 import buildAst from './buildAst.js';
-import stylish from './stylish.js';
+import formatter from './formatters/index.js';
 
-const genDiff = (path1, path2, formatter = stylish) => {
+const genDiff = (path1, path2, formatName = 'stylish') => {
   const parsedData1 = parsers(path1);
   const parsedData2 = parsers(path2);
   const ast = buildAst(parsedData1, parsedData2);
-  return formatter(ast);
+  return formatter(ast, formatName);
 };
 
 export default genDiff;

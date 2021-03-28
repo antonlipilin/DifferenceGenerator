3be42c8825dbbd3a4e5193c8a5ad6b7ec16a88a5
Anton Lipilin <antonlipilinwork@gmail.com>
2021-03-28 19:37:03 +0400

    deleted let

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,13 +2,12 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 
 export default (ast, formatName) => {
-  let result;
   if (formatName === 'stylish') {
-    result = stylish(ast);
-  } else if (formatName === 'plain') {
-    result = plain(ast);
-  } else if (formatName === 'json') {
-    result = JSON.stringify(ast);
+    return stylish(ast);
   }
-  return result;
+  if (formatName === 'plain') {
+    return plain(ast);
+  }
+
+  return JSON.stringify(ast);
 };
--- src/parsers.js
+++ src/parsers.js
@@ -5,11 +5,9 @@ import path from 'path';
 export default (pathToData) => {
   const data = fs.readFileSync(pathToData, 'utf-8');
   const extname = path.extname(pathToData);
-  let parser;
   if (extname === '.json') {
-    parser = JSON.parse(data);
-  } else if (extname === '.yml') {
-    parser = yaml.load(data);
+    return JSON.parse(data);
   }
-  return parser;
+
+  return yaml.load(data);
 };

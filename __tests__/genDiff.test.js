abbf032850f1a3539e71b385de753112005eb5ef
Anton Lipilin <antonlipilinwork@gmail.com>
2021-03-28 14:11:29 +0400

    removed tests and fixtures for flat objects

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -6,30 +6,6 @@ import genDiff from '../src/genDiff.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-test('JSON simple object', () => {
-  expect(genDiff(`${__dirname}/../__fixtures__/file1.json`, `${__dirname}/../__fixtures__/file2.json`))
-    .toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
-});
-
-test('YAML simple object', () => {
-  expect(genDiff(`${__dirname}/../__fixtures__/file1.yml`, `${__dirname}/../__fixtures__/file2.yml`))
-    .toEqual(`{
-  - follow: false
-    host: hexlet.io
-  - proxy: 123.234.53.22
-  - timeout: 50
-  + timeout: 20
-  + verbose: true
-}`);
-});
-
 test('JSON nested object', () => {
   expect(genDiff(`${__dirname}/../__fixtures__/nestedObject1.json`, `${__dirname}/../__fixtures__/nestedObject2.json`)).toEqual(`{
     common: {

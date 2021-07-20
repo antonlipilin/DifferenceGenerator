28828b4b6fe4f1d64177a301349e28bb0df237e1
Anton Lipilin <antonlipilinwork@gmail.com>
2021-07-20 14:13:34 +0400

    fix removed multiple returns &  added a variable to store the result

--- src/buildAst.js
+++ src/buildAst.js
@@ -4,25 +4,28 @@ const buildAst = (obj1, obj2) => {
   const unionKeys = _.union(Object.keys(obj1), Object.keys(obj2));
   const sortedKeys = _.sortBy(unionKeys);
   return sortedKeys.map((key) => {
+    let result;
+
     if (!_.has(obj1, key)) {
-      return { name: key, value: obj2[key], type: 'added' };
+      result = { name: key, value: obj2[key], type: 'added' };
     }
 
     if (!_.has(obj2, key)) {
-      return { name: key, value: obj1[key], type: 'deleted' };
+      result = { name: key, value: obj1[key], type: 'deleted' };
     }
 
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      return { name: key, value: buildAst(obj1[key], obj2[key]), type: 'nested' };
+      result = { name: key, value: buildAst(obj1[key], obj2[key]), type: 'nested' };
     }
 
     if (obj1[key] !== obj2[key]) {
-      return {
+      result = {
         name: key, oldValue: obj1[key], newValue: obj2[key], type: 'changed',
       };
     }
 
-    return { name: key, value: obj1[key], type: 'unchanged' };
+    result = { name: key, value: obj1[key], type: 'unchanged' };
+    return result;
   });
 };
 

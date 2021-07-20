37c96cd51b0447033e49d32cdfd4f80b363da9d8
Anton Lipilin <antonlipilinwork@gmail.com>
2021-07-20 14:16:27 +0400

    Revert "fix removed multiple returns &  added a variable to store the result"
    This reverts commit 28828b4b6fe4f1d64177a301349e28bb0df237e1.

--- src/buildAst.js
+++ src/buildAst.js
@@ -4,28 +4,25 @@ const buildAst = (obj1, obj2) => {
   const unionKeys = _.union(Object.keys(obj1), Object.keys(obj2));
   const sortedKeys = _.sortBy(unionKeys);
   return sortedKeys.map((key) => {
-    let result;
-
     if (!_.has(obj1, key)) {
-      result = { name: key, value: obj2[key], type: 'added' };
+      return { name: key, value: obj2[key], type: 'added' };
     }
 
     if (!_.has(obj2, key)) {
-      result = { name: key, value: obj1[key], type: 'deleted' };
+      return { name: key, value: obj1[key], type: 'deleted' };
     }
 
     if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
-      result = { name: key, value: buildAst(obj1[key], obj2[key]), type: 'nested' };
+      return { name: key, value: buildAst(obj1[key], obj2[key]), type: 'nested' };
     }
 
     if (obj1[key] !== obj2[key]) {
-      result = {
+      return {
         name: key, oldValue: obj1[key], newValue: obj2[key], type: 'changed',
       };
     }
 
-    result = { name: key, value: obj1[key], type: 'unchanged' };
-    return result;
+    return { name: key, value: obj1[key], type: 'unchanged' };
   });
 };
 
